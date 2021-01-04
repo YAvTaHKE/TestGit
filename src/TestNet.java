b7fd86d4e2e5e2fb57a4f9e5682192d1a0056a07
YAvTAHKE_WorkPC <moertiac@gmail.com>
2021-01-04 22:29:54 +0300

    new branch_1

--- src/TestNet.java
+++ src/TestNet.java
@@ -1,6 +1,7 @@
 public class TestNet {
     public static void main(String [] arg) {
         System.out.println("Первая строчка рабочий комп");
+        System.out.println("Новая ветка на WorkPC");
     }
 
 
